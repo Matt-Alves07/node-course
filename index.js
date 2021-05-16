1de33e0992d4372a9b88679ed8364d099b9eb761
Matt-Alves07 <mathalves07.winstore@outlook.com>
2021-05-16 10:55:01 -0300

    Implementei a particionalização de código no projeto

---
+++ index.js
@@ -0,0 +1,9 @@
+var sum = require('./js/sum');
+var sub = require('./js/sub');
+var div = require('./js/div');
+var mult = require('./js/mult');
+
+console.log(sum(10,5));
+console.log(sub(20,5));
+console.log(div(30,5));
+console.log(mult(15,5));
